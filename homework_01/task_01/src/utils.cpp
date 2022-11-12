d6f6f05d058de02366db0e8ef07aaf0b90010344
Aynur <nuryyewaaynur03@gmail.com>
2022-11-12 08:34:47 +0000

    wip

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -2,6 +2,17 @@
 
 #include <stack>
 
-std::vector<std::string> SplitString(const std::string& data) {
-  return {data};
+std::vector<std::string> SplitString(const std::string &data) {
+  std::string a = "";
+  std::vector<std::string> b;
+  for (auto x : data) {
+    if (x == ' ') {
+      b.push_back(a);
+      a = "";
+    } else {
+      a = a + x;
+    }
+  }
+  b.push_back(a);
+  return {b};
 }
