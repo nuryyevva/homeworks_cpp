8e132f8112742f9063934c218fb4310e0e6f77a9
Aynur <nuryyewaaynur03@gmail.com>
2022-11-24 16:43:18 +0000

    did the homework

--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -2,6 +2,30 @@
 
 #include <stack>
 
-int Calculate(const std::string& data) {
+int Calculate(const std::string &data) {
+  std::string helper = "";
+  std::vector<std::string> nums_to_calc;
+  for (auto x : data) {
+    if (x == '+' || x == '-' || x == '*') {
+      nums_to_calc.push_back(helper);
+      helper = "";
+    } else {
+      helper = helper + x;
+    }
+  }
+  nums_to_calc.push_back(helper);
+  int m = std::stoi(nums_to_calc[0]);
+  int n = std::stoi(nums_to_calc[1]);
+  for (auto x : data) {
+    if (x == '+') {
+      return (m + n);
+    }
+    if (x == '-') {
+      return (m - n);
+    }
+    if (x == '*') {
+      return (m * n);
+    }
+  }
   return 0;
 }
